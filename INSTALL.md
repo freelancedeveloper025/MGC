24558eb21ff1a41436d5ef8ea07b41bf2a76d8f4
Original-Tasty <quentin.neveu@hotmail.ca>
2021-09-24 07:24:46 -0400

    build fix part 1/4

--- INSTALL.md
+++
@@ -1,5 +0,0 @@
-Building Mgc
-=============
-
-See doc/build-*.md for instructions on building the various
-elements of the Mgc Core reference implementation of Mgc.
--- contrib/auto_gdb/mgc_dbg.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# use testnet settings,  if you need mainnet,  use ~/.mgccore/mgcd.pid file instead
-export LC_ALL=C
-
-mgc_pid=$(<~/.mgccore/testnet3/mgcd.pid)
-sudo gdb -batch -ex "source debug.gdb" mgcd ${mgc_pid}
